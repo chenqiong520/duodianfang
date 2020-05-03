1e2c9e379e0dfc293ca326e4d6b241b4bf87fa47
chenqiong <741718030@qq.com>
2020-05-03 08:55:57 +0800

    初始化

--- middleware/redirect.js
+++
@@ -1,7 +0,0 @@
-export default context => {
-  const { route, redirect } = context
-  // 路由重定向
-  if (route.fullPath === '/index' || route.fullPath === '/home') {
-    redirect('/')
-  }
-}
--- plugins/cipboard.js
+++
@@ -1,5 +0,0 @@
-// 复制插件
-import Vue from 'vue'
-import VueClipboard from 'vue-clipboard2'
-
-Vue.use(VueClipboard)
